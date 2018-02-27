93d7e5e92d811177ca631ccfced767b3d217cb2a
theNuclearCodes <36515323+theNuclearCodes@users.noreply.github.com>
2018-02-27 13:48:16 -0500

    This is the header file for the processing code

---
+++ multiplicityStructure.h
@@ -0,0 +1,45 @@
+#ifndef binaryFileStruct
+#define binaryFileStruct
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdint.h>
+
+class FissLine
+{
+public:
+  int history;
+  int generation;
+  int multiplicityNeutrons;
+  int multiplicityPhotons;
+};
+
+class CollLine
+{
+public:
+  int history;
+  int generation;
+  int multiplicityNeutrons;
+  int multiplicityPhotons;
+};
+
+class HistoryClass
+{
+public:
+  int history;
+  int number;
+  int yype;
+  int interaction;
+  int zaid;
+  int cell;
+  double energy;
+  double time;
+  double position[3];
+  double weight;
+  int generation;
+  int  scattersCode;
+  double prior;
+};
+
+
+#endif
